7f646a4799ac32051adead2d2c6cccf69deebae5
Tranminh238 <minhnhi23082004@gmail.com>
2025-10-31 13:47:56 +0700

    register

--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -1,7 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.auth.request.RegistRequest;
+import com.example.demo.entity.Client;
+import com.example.demo.entity.User;
+import com.example.demo.exception.Exception;
+import com.example.demo.repository.ClientRepository;
+import com.example.demo.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ClientService {
+    private final ClientRepository clientRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void registerClient(RegistRequest registRequest) {
+        if(userRepository.findByUsername(registRequest.getUsername()).isPresent()){
+            throw new Exception("User đã tồn tại");
+        }
+        User userEntity = User.builder()
+                .username(registRequest.getUsername())
+                .password(passwordEncoder.encode(registRequest.getPassword()))
+                .role("CLIENT")
+                .build();
+        User user = userRepository.save(userEntity);
+
+        Client client = Client.builder()
+                .userId(user.getId())
+                .fullName(registRequest.getFullName())
+                .address(registRequest.getAddress())
+                .build();
+        clientRepository.save(client);
+    }
 }
